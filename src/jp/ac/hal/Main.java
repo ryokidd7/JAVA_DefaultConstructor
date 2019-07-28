c9c71ed322575e2b8c5dfce74a484187e56d54cc
ryoki726 <ryooki.0726@gmail.com>
2019-07-28 22:22:24 +0900

    実行結果コメント

--- src/jp/ac/hal/Main.java
+++ src/jp/ac/hal/Main.java
@@ -15,17 +15,23 @@ class Book {
 	}
 
 	void print() {
-		System.out.println(" 名前:" + title);
-		System.out.println(" 趣味:" + writer);
+		System.out.println(" タイトル:" + title);
+		System.out.println(" 名前:" + writer);
 	}
 }
 
 class Books {
 	public static void main(String[] args) {
-		Book book1 = new Book(" 私は","ジャバ");
+		Book book1 = new Book(" 頑張りたい","ジャバ");
 		book1.print();
 		Book book2 = new Book(book1);
 		book2.title = "勉強してるのは";
 		book2.print();
 	}
+		//実行結果
+		//タイトル: 頑張りたい
+		//名前:ジャバ
+		//タイトル:勉強してるのは
+		//名前:ジャバ
+
 }

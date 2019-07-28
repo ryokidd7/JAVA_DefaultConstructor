50c1c76c792a9e6cb52e6ff75cb433e7b22c901f
ryoki726 <ryooki.0726@gmail.com>
2019-07-28 22:18:16 +0900

    コンストラクタを呼び出し、結果を出す

--- src/jp/ac/hal/Main.java
+++ src/jp/ac/hal/Main.java
@@ -14,4 +14,18 @@ class Book {
 		writer = copy.writer;
 	}
 
+	void print() {
+		System.out.println(" 名前:" + title);
+		System.out.println(" 趣味:" + writer);
+	}
+}
+
+class Books {
+	public static void main(String[] args) {
+		Book book1 = new Book(" 私は","ジャバ");
+		book1.print();
+		Book book2 = new Book(book1);
+		book2.title = "勉強してるのは";
+		book2.print();
+	}
 }
